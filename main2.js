c6532f13eac242552864907f8deccf2928848fa6
Woody Ramey <woodyramey@icloud.com>
2023-07-17 22:47:54 -0500

    Add files via upload

--- main2.js
+++ main2.js
@@ -26,3 +26,28 @@ function drinkSide(){
 }
 drinkSide();
 
+//2nd project
+let colors = document.querySelector(".colors");
+function colored(){
+    colors.addEventListener("mouseover",() =>{
+    document.querySelector('.overlay2').style.display = 'block';
+    document.querySelector('.colorsImg').style.filter = 'brightness(60%)';
+    })
+
+    colors.addEventListener("mouseout",() => {
+        document.querySelector('.overlay2').style.display = '';
+        document.querySelector('.colorsImg').style.filter = '';
+    });
+};
+colored()
+
+function drinkSide2(){
+    colors.addEventListener('click',() =>{
+        document.getElementById('sideModal2').classList.toggle('hidden');
+    });
+    let sidemArrow = document.getElementById('sidemArrow2');
+    sidemArrow.addEventListener('click',() =>{
+        document.getElementById('sideModal2').classList.toggle('block');
+    });
+}
+drinkSide2();
