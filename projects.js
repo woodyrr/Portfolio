a27386be9df9b1a47799a8346a13d19697ac1723
Woody Ramey <woodyramey@icloud.com>
2023-09-24 21:48:16 -0500

    Add files via upload

--- projects.js
+++ projects.js
@@ -1,28 +1,19 @@
-
-
-
-
-
-
-
 let openBtn = document.querySelector('.nav-open-btn')
-let openBtn1 = document.querySelector('.nav-open-btn1')
 let line = document.querySelectorAll('.line')
 let navdefault = document.querySelector('.navbar-default')
 let headova = document.querySelector('.header-overlay')
 let body = document.querySelector('.body')
+let openBtn1 = document.querySelector('.nav-open-btn1')
 openBtn.addEventListener("click", mobileMenu);
 openBtn1.addEventListener("click", mobileMenu1);
-
 function mobileMenu() {
     openBtn.classList.toggle("active");
     headova.classList.toggle("active");
-    body.classList.toggle("overflow-hidden")
-    
+    body.classList.toggle("overflow-hidden");
 }
 function mobileMenu1() {
     openBtn1.classList.toggle("active");
     headova.classList.toggle("active");
     body.classList.toggle("overflow-hidden")
-    
 }
+
